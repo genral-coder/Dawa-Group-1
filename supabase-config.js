6b187f5d09deb6e1b64fd94e22c890a575e26a6a
genral-coder <ebrahemgamer10@gmail.com>
2026-08-09 18:52:49 +0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,8 +0,0 @@
-/* DAWA Group - Supabase config
-   املأ القيمتين دول بعد ما تعمل مشروع على supabase.com:
-   Project Settings > API  →  Project URL  و  anon / public key
-   لو سبتهم فاضيين الموقع هيشتغل عادي بالبيانات الافتراضية (بدون أدمن). */
-window.SUPABASE = {
-  url: "https://xykbriwemyadftaghkig.supabase.co",
-  anonKey: "sb_publishable_qz5ybDWFnanwUftWUv7EHg_kYxMjVIG"
-};
